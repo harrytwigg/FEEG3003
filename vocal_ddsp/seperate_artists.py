a9acd84326419be6c1f52a2149af1b716c1a45e8
Harry Twigg <harrytwigg111@gmail.com>
2022-04-19 14:11:34 +0100

    Before sync

--- vocal_ddsp/seperate_artists.py
+++ vocal_ddsp/seperate_artists.py
@@ -3,4 +3,4 @@ from vocaliser.seperator import seperate_artists
 if __name__ == '__main__':
     '''seperate_artists(['Taylor Swift', 'Coldplay', 'Adele', 'Ellie Golding',
                      'James Blunt'])'''
-    seperate_artists(['Capaldi'])
+    seperate_artists(['Birdy'])
